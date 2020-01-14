f5c9b3502935e86e90b9858e4025fc755f8508b7
Failip98 <filip98@spray.se>
2020-01-14 13:14:23 +0100

    test

--- app/src/main/java/com/example/marvellisimo/MainActivity.kt
+++ app/src/main/java/com/example/marvellisimo/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    // dd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
