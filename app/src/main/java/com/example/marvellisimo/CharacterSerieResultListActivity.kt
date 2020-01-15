0b8a2fefc999c8a1fde34923d4ebd4f678624741
Rami <rami.albadri77@gmail.com>
2020-01-15 13:29:43 +0100

    bind each item with some data

--- app/src/main/java/com/example/marvellisimo/CharacterSerieResultListActivity.kt
+++ app/src/main/java/com/example/marvellisimo/CharacterSerieResultListActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_character_serie_result_list.*
+import kotlinx.android.synthetic.main.search_result_item.view.*
 
 class CharacterSerieResultListActivity : AppCompatActivity() {
 
@@ -40,6 +42,17 @@ class SearchResultItem: Item<GroupieViewHolder>() {
     }
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.search_result_item_description_textView.text =
+            "Here is some information about this serie/character sijdfcos sofjorfi owejnfowienf"
+
+        viewHolder.itemView.search_result_item_name_textView.text =
+            "Breaking Bad"
+
+        val uri = "https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+
+
+        Picasso.get().load(uri).into(viewHolder.itemView.search_result_item_imageView)
+
     }
 
 }
