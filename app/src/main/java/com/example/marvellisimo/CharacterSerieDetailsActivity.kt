a0311fb0912245876b488e837cdff7726aa146f3
Rami <rami.albadri77@gmail.com>
2020-01-16 10:19:35 +0100

    detail activity option menu

--- app/src/main/java/com/example/marvellisimo/CharacterSerieDetailsActivity.kt
+++ app/src/main/java/com/example/marvellisimo/CharacterSerieDetailsActivity.kt
@@ -3,6 +3,9 @@ package com.example.marvellisimo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.marvellisimo.ui.entities.Serie
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_character_serie_details.*
@@ -24,5 +27,28 @@ class CharacterSerieDetailsActivity : AppCompatActivity() {
 
         Log.d("onRecieve", selectedResultItem.name)
         Log.d("onRecieve", selectedResultItem.description)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.selected_item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.detail_menu_send ->{
+                Toast.makeText(getApplicationContext(), "You clicked Send to friend",
+                    Toast.LENGTH_LONG).show()
+
+            }
+            R.id.detail_menu_add_to_favorites ->{
+                Toast.makeText(
+                    applicationContext, "You clicked add to favorites",
+                    Toast.LENGTH_LONG).show()
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
