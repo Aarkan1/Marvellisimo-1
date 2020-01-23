09ae10c790b13af2b086ac6678d2446797d5011b
Johan <johanwiren1@gmail.com>
2020-01-23 19:35:48 +0100

    Refactored null values

--- app/src/main/java/com/example/marvellisimo/models/User.kt
+++ app/src/main/java/com/example/marvellisimo/models/User.kt
@@ -1,21 +1,17 @@
 package com.example.marvellisimo.models
 
-
 import io.realm.RealmObject
 import io.realm.annotations.Ignore
 import io.realm.annotations.PrimaryKey
 
 open class User : RealmObject() {
     @PrimaryKey
-    var uid: String? = null
-    var username: String? = null
-    var avatar: String? = null
+    var uid: String = ""
+    var username: String = ""
+    var avatar: String = ""
+    var isOnline: Boolean = false
     @Ignore
-    var favoriteCharacters: ArrayList<String>? = null
+    var favoriteCharacters: ArrayList<String> = ArrayList()
     @Ignore
-    var favoriteSeries: ArrayList<String>? = null
-//  one-to-many relationship to other RealmObjects
-//  var ownedDogs: RealmList<Dog> = RealmList()
+    var favoriteSeries: ArrayList<String> = ArrayList()
 }
-
-//data class User(var uid: String = "", var username: String = "", var avatar: String = "")
