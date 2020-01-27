023830e97703f111b62cf03f6b685c13fc8fdcc1
Johan <johanwiren1@gmail.com>
2020-01-27 21:20:43 +0100

    Updated marvel entity to load character urls

--- app/src/main/java/com/example/marvellisimo/marvelEntities/Entities.kt
+++ app/src/main/java/com/example/marvellisimo/marvelEntities/Entities.kt
@@ -1,12 +1,8 @@
 package com.example.marvellisimo.marvelEntities
 
-import android.os.Parcelable
 import io.realm.RealmList
 import io.realm.RealmObject
-import io.realm.annotations.Ignore
 import io.realm.annotations.PrimaryKey
-import kotlinx.android.parcel.Parcelize
-import org.jetbrains.annotations.NotNull
 
 data class CharacterDataWrapper(
     // val copyright: String, // optional): The copyright notice for the returned result.,
@@ -29,7 +25,7 @@ data class CharacterDataContainer(
 open class Character(
     //val modified: Date, //, optional): The date the resource was most recently modified.,
     //val resourceURI: string, //, optional): The canonical URL identifier for this resource.,
-    //val urls: Array, //[Url], optional): A set of public web site URLs for the resource.,
+    var urls: RealmList<Url> = RealmList(), //[Url], optional): A set of public web site URLs for the resource.,
     var thumbnail: Image? = Image("", ""), //, optional): The representative image for this character.,
     /*val comics: ComicList, //, optional): A resource list containing comics which feature this character.,
     val stories: StoryList, //, optional): A resource list of stories in which this character appears.,
@@ -40,6 +36,11 @@ open class Character(
     var id: Int = 1, //, optional): The unique ID of the character resource.,
     var name: String = "", //, optional): The name of the character.,
     var description: String = "" //, optional): A short bio or description of the character.,
+): RealmObject()
+
+open class Url (
+    //var type (string, optional): A text identifier for the URL.,
+    var url: String = "" // (string, optional): A full URL (including scheme, domain, and path).
 ): RealmObject()
 
 open class SeriesList (
